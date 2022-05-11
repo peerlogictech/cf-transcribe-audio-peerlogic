ff861739c6e71381ca083fc01c53483461ea0df1
Alex Maskovyak <alexander.maskovyak@gmail.com>
2022-05-11 09:19:11 -0700

    Update configuration to support the new env var names

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import base64
 from typing import Optional
-from config import BUCKET_AUDIO_PCM_ENCODED, BUCKET_GOOGLE_SPEECH_TO_TEXT_RAW_EXTRACT
+from config import BUCKET_OUTPUT_AUDIO_PCM_ENCODED, BUCKET_OUTPUT_RAW_EXTRACT
 import json
 import logging
 
@@ -112,12 +112,12 @@ def transcribe_audio_peerlogic_pubsub(event, context):
     log.info("Converted in memory wavefile to pcm")
 
     # PCM is still wav extension: https://trac.ffmpeg.org/wiki/audio%20types
-    log.info(f"Saving local pcm encoded file {partial_id}.wav to bucket {BUCKET_AUDIO_PCM_ENCODED}")
-    pcm_file_gs_uri = upload_file_to_bucket(f"{partial_id}.wav", pcm_file_path, bucket_name=BUCKET_AUDIO_PCM_ENCODED)
-    log.info(f"Saved local pcm encoded file to bucket {BUCKET_AUDIO_PCM_ENCODED}")
+    log.info(f"Saving local pcm encoded file {partial_id}.wav to bucket {BUCKET_OUTPUT_AUDIO_PCM_ENCODED}")
+    pcm_file_gs_uri = upload_file_to_bucket(f"{partial_id}.wav", pcm_file_path, bucket_name=BUCKET_OUTPUT_AUDIO_PCM_ENCODED)
+    log.info(f"Saved local pcm encoded file to bucket {BUCKET_OUTPUT_AUDIO_PCM_ENCODED}")
 
     # Transcribe and specify destination for output using call partial id
-    destination_uri = f"gs://{BUCKET_GOOGLE_SPEECH_TO_TEXT_RAW_EXTRACT}/{call_id}-{partial_id}-{audio_partial_id}.json"
+    destination_uri = f"gs://{BUCKET_OUTPUT_RAW_EXTRACT}/{call_id}-{partial_id}-{audio_partial_id}.json"
     log.info(f"Beginning long-running transcription of pcm encoded wave file using Google Speech to Text.")
     transcribe_model_selection(pcm_file_gs_uri, destination_uri, sample_rate_hertz=sample_rate)
     # TODO: See if timeouts mean failure and this will reprocess from dead-letter queue
